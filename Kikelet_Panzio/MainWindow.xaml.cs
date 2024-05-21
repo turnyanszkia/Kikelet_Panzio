df496e7b55d59ef469f5918401e764c62be8a51e
turnyanszkia <turnyanszkia@kkszki.hu>
2024-05-21 11:39:54 +0200

    Add project files.

---
+++ Kikelet_Panzio/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Kikelet_Panzio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Szoba> szobak = new List<Szoba> ();
+        public MainWindow()
+        {
+            InitializeComponent();
+            CbxFerohey.Items.Clear();
+            CbxFerohey.Items.Add("2 férőhely");
+            CbxFerohey.Items.Add("3 férőhely");
+            CbxFerohey.Items.Add("4 férőhely");
+
+            CbxFerohey.SelectedIndex = 0;
+            LoadFromFile("szobak.txt"); 
+        }
+
+        private void LoadFromFile(string fileName)
+        {
+            string[] sorok = File.ReadAllLines(fileName);
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                szobak.Add(new Szoba(sorok[i]));
+            }
+        }
+
+        private void CbxFerohey_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void BtnMentes_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Kikelet_Panzio/Szoba.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kikelet_Panzio
+{
+    internal class Szoba
+    {
+        int szobaszam;
+        int ferohely;
+        int ar;
+
+        public Szoba(int szobaszam, int ferohely, int ar)
+        {
+            Szobaszam = szobaszam;
+            Ferohely = ferohely;
+            Ar = ar;
+        }
+
+        public int Szobaszam { get => szobaszam; set => szobaszam = value; }
+        public int Ferohely { get => ferohely; set => ferohely = value; }
+        public int Ar { get => ar; set => ar = value; }
+
+        public Szoba(string sor)
+        {
+            string[] bontas = sor.Split(';');
+            Szobaszam = int.Parse(bontas[0]);
+            Ferohely = int.Parse(bontas[1]);
+            Ar = int.Parse(bontas[2]);
+        }
+    }
+}
